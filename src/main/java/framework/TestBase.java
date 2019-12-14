c51f1913d01e2807a5237c679ef2e5806fa5fb48
grozagrozaalinioan <groza.alin.ioan@gmail.com>
2019-12-14 15:54:16 +0200

    Refactored pages

--- src/main/java/framework/TestBase.java
+++ src/main/java/framework/TestBase.java
@@ -5,12 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class TestBase {
     protected static WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "c:\\Selenium\\chromedriver\\chromedriver.exe");
+        getDriverPath();
+        System.setProperty("webdriver.chrome.driver", getDriverPath());
         driver = new ChromeDriver();
     }
 
@@ -23,4 +29,25 @@ public class TestBase {
         driver.close();
         driver.quit();
     }
+
+    private String getDriverPath() {
+        InputStream inputStream = getClass()
+                .getClassLoader().getResourceAsStream("driverPath.txt");
+        InputStreamReader isReader = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(isReader);
+        StringBuffer sb = new StringBuffer();
+        String str = null;
+        while (true) {
+            try {
+                if (!((str = reader.readLine()) != null)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            sb.append(str);
+        }
+        return sb.toString();
+
+    }
+
+
 }
